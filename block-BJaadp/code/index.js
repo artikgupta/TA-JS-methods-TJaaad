f427d2f2f6b0c8113ae835e1aecd7980ff5dc986
artikgupta <artig9262@gmail.com>
2020-12-12 15:58:00 +0530

    complete block 2 of assignment level III

--- block-BJaadp/code/index.js
+++ block-BJaadp/code/index.js
@@ -1,23 +1,50 @@
 let persons = [
-  { name: 'John', grade: 8, sex: 'M' },
-  { name: 'Sarah', grade: 12, sex: 'F' },
-  { name: 'Bob', grade: 16, sex: 'M' },
-  { name: 'Johnny', grade: 2, sex: 'M' },
-  { name: 'Ethan', grade: 4, sex: 'M' },
-  { name: 'Paula', grade: 18, sex: 'F' },
-  { name: 'Donald', grade: 5, sex: 'M' },
-  { name: 'Jennifer', grade: 13, sex: 'F' },
-  { name: 'Courtney', grade: 15, sex: 'F' },
-  { name: 'Jane', grade: 9, sex: 'F' },
-  { name: 'John', grade: 17, sex: 'M' },
-  { name: 'Arya', grade: 14, sex: 'F' },
+  { name: "John", grade: 8, sex: "M" },
+  { name: "Sarah", grade: 12, sex: "F" },
+  { name: "Bob", grade: 16, sex: "M" },
+  { name: "Johnny", grade: 2, sex: "M" },
+  { name: "Ethan", grade: 4, sex: "M" },
+  { name: "Paula", grade: 18, sex: "F" },
+  { name: "Donald", grade: 5, sex: "M" },
+  { name: "Jennifer", grade: 13, sex: "F" },
+  { name: "Courtney", grade: 15, sex: "F" },
+  { name: "Jane", grade: 9, sex: "F" },
+  { name: "John", grade: 17, sex: "M" },
+  { name: "Arya", grade: 14, sex: "F" },
 ];
 
 // NOTE: Use reduce method whereever you can to solve this exercise:
 
 // Find the average grade
 
+let TotalGrade = persons.reduce((acc, cv) => {
+  return acc + cv.grade;
+}, 0);
+
+let length = persons.map((v) => v.grade).length;
+
+console.log(length);
+
+let averageGrade = TotalGrade / length;
+
+console.log(averageGrade);
+
 // Find the average grade of male
+
+let male = persons.filter((v) => v.sex === "M");
+
+console.log(male);
+
+let maleGrade = persons
+  .filter((v) => v.sex === "M")
+  .reduce((acc, cv) => {
+    acc + cv.grade;
+    return acc;
+  }, []);
+
+let averageMaleMarks = maleGrade / male.length;
+
+console.log(averageMaleMarks);
 
 // Find the average grade of female
 
@@ -30,17 +57,17 @@ let persons = [
 // Find the highest grade for people whose name starts with 'J' or 'P'
 
 const fruitBasket = [
-  'banana',
-  'cherry',
-  'orange',
-  'apple',
-  'cherry',
-  'orange',
-  'apple',
-  'banana',
-  'cherry',
-  'orange',
-  'fig',
+  "banana",
+  "cherry",
+  "orange",
+  "apple",
+  "cherry",
+  "orange",
+  "apple",
+  "banana",
+  "cherry",
+  "orange",
+  "fig",
 ];
 
 /* 
@@ -48,10 +75,16 @@ const fruitBasket = [
 Use the fruitBasket array to create an object where key will be the fruit and value will be the number of times
 that fruit has appeared in the array. Store it in new variable fruitsObj
 
+
 Output: 
 {banana: 2, cherry: 3, orange: 3, apple: 2, fig: 1}
 */
 
+let fruitsObj = fruitBasket.reduce((acc, cv) => {
+  acc = acc[cv] = cv.lastIndexOf(cv);
+
+  return acc;
+}, {});
 /* 
 
 Use the fruitBasket array to create an array of array. Each array will contain two values name of fruit and number of times
@@ -71,6 +104,11 @@ const data = [
 
 // Using reduce flat data array
 
+// let flatData = data.reduce((acc, cv) => {
+//   acc = cv.map((v) => v.flat(3));
+//   return acc;
+// });
+
 const dataTwo = [
   [1, 2, 3],
   [4, 5, 6],
@@ -80,8 +118,12 @@ const dataTwo = [
 
 // Using reduce flat dataTwo array
 
-/*
+// let flatData2 = dataTwo.reduce((acc, cv) => {
+//   acc = cv.map((v) => v.flat(infinity));
+//   return acc;
+// });
 
+/*
 Create these functions which accepts a number value and returns a number value:
   - `increment` adds one to the input value
   - `double` doubles the input value
@@ -115,18 +157,18 @@ EXAMPLE:
   ...
 */
 
-let pipeline2 = [
-  increment,
-  half,
-  double,
-  decrement,
-  decrement,
-  triple,
-  double,
-  triple,
-  half,
-  increment,
-  triple,
-];
+// let pipeline2 = [
+//   increment,
+//   half,
+//   double,
+//   decrement,
+//   decrement,
+//   triple,
+//   double,
+//   triple,
+//   half,
+//   increment,
+//   triple,
+// ];
 
 // Find the output using pipeline2 the initial value if 8
